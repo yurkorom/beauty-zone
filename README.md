f3ce9180a0f4919f0603fa7cefb43eb946b302b6
Roman Yurko <yurko.rom@gmail.com>
2024-09-24 16:07:16 +0300

    update project

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-[Homework 1](https://yurkorom.github.io/beauty-zone/homework-1/)<br>
-[Homework 2](https://yurkorom.github.io/beauty-zone/homework-2/)<br>
-[Homework 3](https://yurkorom.github.io/beauty-zone/homework-3/)<br>
-[Homework 4](https://yurkorom.github.io/beauty-zone/homework-4/)<br>
-Final project (to be completed)
+# genius-beauty-zone
--- homework-3/js/modal.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
-
-const toggleModal = () => modal.classList.toggle('is-hidden');
-
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
